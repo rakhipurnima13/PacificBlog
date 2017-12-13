2b59bcc6e2203ddd953d9e75838a02e22d0d424c
Purnima <rakhipurnima13@outlook.com>
2017-12-13 23:25:21 +0530

    Add project files.

---
+++ PacificBlog/Controllers/BlogsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PacificBlog.Models;
+
+namespace PacificBlog.Controllers
+{
+    public class BlogsController : Controller
+    {
+        // GET: Blogs
+        public BlogModel BlogPage()
+        {
+            BlogModel BlogDetails = new BlogModel();
+            BlogDetails.BlogTitle = "Test Title";
+            BlogDetails.Writer = "By Anonymous";
+            BlogDetails.Content = "Test Content:";
+            return BlogDetails;            
+        }
+
+        public ActionResult CreateBlog()
+        {
+            return View();
+        }
+    }
+}
---
+++ PacificBlog/Models/BlogModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PacificBlog.Models
+{
+    public class BlogModel
+    {
+
+        public string BlogTitle {get; set;}
+
+        public string Writer { get; set; }
+
+        public string Content { get; set; }
+    }
+}
